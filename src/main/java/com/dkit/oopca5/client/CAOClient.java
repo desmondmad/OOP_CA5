ebf80de014f7eb30d66cd8ef992f9ef6fdc67170
desmond madden <desmondmad19@gmail.com>
2021-03-26 10:42:59 +0000

    2nd Commit Added logged In menu

--- src/main/java/com/dkit/oopca5/client/CAOClient.java
+++ src/main/java/com/dkit/oopca5/client/CAOClient.java
@@ -7,10 +7,8 @@ package com.dkit.oopca5.client;
  */
 
 import com.dkit.oopca5.core.CAOService;
-import com.dkit.oopca5.server.MySqlStudentDao;
-import com.dkit.oopca5.server.StudentDaoInterface;
-import com.dkit.oopca5.server.MySqlCourseDao;
-import com.dkit.oopca5.server.CourseDaoInterface;
+import com.dkit.oopca5.core.DTO.Student;
+import com.dkit.oopca5.server.*;
 
 import java.util.Scanner;
 
@@ -19,6 +17,13 @@ public class CAOClient
     private static final int QUIT = 0;
     private static final int REGISTER = 1;
     private static final int LOGIN = 2;
+
+    private static final int LOGOUT = 1;
+    private static final int DISPLAY_COURSE = 2;
+    private static final int DISPLAY_ALL_COURSES = 3;
+    private static final int DISPLAY_CURRENT_CHOICES = 4;
+    private static final int UPDATE_CURRENT_CHOICES = 5;
+
     Scanner keyboard = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -42,11 +47,52 @@ public class CAOClient
                     break;
                 case LOGIN:
                     System.out.println("Enter Login details");
-
-                    break;
+                    System.out.println("Please enter your caoNumber: ");
+                    int caoNumber = keyboard.nextInt();
+                    keyboard.nextLine();
+                    System.out.println("Please enter your date of birth (yyyy-mm-dd): ");
+                    String dateString = keyboard.nextLine();
+                    System.out.println("Please enter your password: ");
+                    String password = keyboard.nextLine();
+                    StudentDaoInterface studentdao = new MySqlStudentDao();
+                    Student s = new Student(caoNumber,dateString,password);
+                    try {
+                        if(studentdao.login(s)){
+                            loggedInMenu(s);
+                        }
+                        break;
+                    } catch (DaoException throwables) {
+                        throwables.printStackTrace();
+                    }
             }
             printStartMenu();
             menuOption = getMenuOption();
+        }
+        System.out.println("Quitting App");
+    }
+
+    private void loggedInMenu(Student s){
+        printMainMenu();
+        int menuOption = getMainMenuOption();
+        while(menuOption != QUIT){
+            switch(menuOption){
+                case LOGOUT:
+                    System.out.println("Enter register details");
+                    register();
+                    break;
+                case DISPLAY_COURSE:
+                    System.out.println("Enter Course ID");
+                    String courseid = keyboard.nextLine();
+                    break;
+                case DISPLAY_ALL_COURSES:
+                    break;
+                case DISPLAY_CURRENT_CHOICES:
+                    break;
+                case UPDATE_CURRENT_CHOICES:
+                    break;
+            }
+            printMainMenu();
+            menuOption = getMainMenuOption();
         }
         System.out.println("Quitting App");
     }
@@ -82,6 +128,18 @@ public class CAOClient
         return option;
     }
 
+    private int getMainMenuOption(){
+        int option;
+        System.out.println("Please enter option: ");
+        option = keyboard.nextInt();
+        while(option != 0 && option != 1 && option != 2 && option != 3 && option != 4 && option != 5)
+        {
+            System.out.println("Please enter a valid number: ");
+            option = keyboard.nextInt();
+        }
+        return option;
+    }
+
     private void register(){
         System.out.println("Please enter caoNumber: ");
         int caoNumber = keyboard.nextInt();
@@ -96,5 +154,7 @@ public class CAOClient
                 dateString + CAOService.BREAKING_CHARACTER +
                 password;
         System.out.println("Message ready to send to server: " + message);
+//        StudentDaoInterface studentdao = new MySqlStudentDao();
+//        if(studentdao.registerStudent())
     }
 }
